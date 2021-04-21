5b0977e2d2caa95b75049cc6165dfa721cf6c615
Yash Rastogi <yashrastogi@gmail.com>
2021-04-21 13:30:10 +0530

    Time: 672 ms (93.38%), Space: 19.4 MB (53.31%) - LeetHub

--- course-schedule-iii/course-schedule-iii.py
+++ course-schedule-iii/course-schedule-iii.py
@@ -1,5 +1,18 @@
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
+        # we start by sorting the courses array in increasing order of deadline, this is done
+        # to ensure that we are able to take up the maximum courses as we traverse the sorted
+        # array serially without exhausting the deadline
+        
+        # we use max heap concept which will always keep the longest duration
+        # course within the heap at the top to pop. we replace the longest course
+        # with current course, if it is of a shorter duration which could allow us to
+        # take more courses as we traverse further
+        
+        # when pushing/popping from min heap pq, we multiply by -1 to reverse
+        # the order of elements in the heap which makes the min heap
+        # act as max heap for our usecase
+        
         courses = sorted(courses, key=lambda x: x[1])
         pq = []; heapify(pq)
         time = 0
@@ -11,6 +24,5 @@ class Solution:
             elif pq and -1 * pq[0] > course[0]:
                 time += course[0] - -1 * heappop(pq)
                 heappush(pq, -1 * course[0])
-        
+
         return len(pq)
-        
