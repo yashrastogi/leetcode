8d36d26012b7e7118e5a9cd2a361b547dc46a169
Yash Rastogi <yashrastogi@gmail.com>
2021-05-13 17:39:15 +0530

    Time: 80 ms (98.17%), Space: 14.3 MB (54.17%) - LeetHub

--- 4sum/4sum.py
+++ 4sum/4sum.py
@@ -1,25 +1,39 @@
 class Solution:
-    def fourSum(self, n: List[int], target: int) -> List[List[int]]:
-        n.sort()
-        a = 0
-        ret = set()
-        while a < len(n):
-            b = a+1
-            while b < len(n):
-                c_sum = n[a]+n[b]
-                c = b+1
-                d = len(n)-1
-                while c < d:
-                    c_sum_2 = c_sum + n[c] + n[d]
-                    if c_sum_2 == target:
-                        ret.add((n[a],n[b],n[c],n[d]))
-                        c += 1
-                        d -= 1
-                    if c_sum_2 < target:
-                        c += 1
-                    elif c_sum_2 > target:
-                        d -= 1
-                b += 1
-            a += 1
+    def fourSum(self, nums: List[int], target) -> List[List[int]]:
+        nums.sort()
+        return self.kSum(nums, start=0, k=4, target=target)
+    
+    def twoSum(self, nums: List[int], start, target: int) -> List[List[int]]:
+        res = []
+        lo, hi = start, len(nums) - 1
+        while (lo < hi):
+            sum = nums[lo] + nums[hi]
+            if sum < target or (lo > start and nums[lo] == nums[lo - 1]):
+                lo += 1
+            elif sum > target or (hi < len(nums) - 1 and nums[hi] == nums[hi + 1]):
+                hi -= 1
+            else:
+                res.append([nums[lo], nums[hi]])
+                lo += 1
+                hi -= 1
+        return res
+    
+    def kSum(self, nums, start, k, target):
+        if start > len(nums) - k + 1:
+            return
+        if nums[start] * k > target or nums[-1] * k < target:
+            return
+        if k == 2:
+            return self.twoSum(nums, start, target)
         
+        ret = []
+        i = start
+        while i < len(nums):
+            while i+1 < len(nums) and i > start and nums[i] == nums[i-1]:
+                i += 1
+            curr = self.kSum(nums, i+1, k-1, target - nums[i])
+            if curr:
+                for pair in curr:
+                    ret.append([nums[i]] + pair)
+            i += 1
         return ret
