e26da419da578dcc7facfa31a2cbc6bba334cdb6
Yash Rastogi <yashrastogi@gmail.com>
2021-05-21 21:13:34 +0530

    Time: 685 ms (5.01%), Space: 40.6 MB (49.63%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.java
+++ set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,42 +1,76 @@
 class Solution {
-    public class Tuple<X, Y> {
-        public final X x;
-        public final Y y;
-        public Tuple(X x, Y y) {
-            this.x = x;
-            this.y = y;
-        }
-        public int hashCode() {
-            return this.x.hashCode() ^ this.y.hashCode();
-        }
-        public String toString() {
-            return "(" + this.x + ", " + this.y + ")";
-        }
-    }
-    
     public void setZeroes(int[][] matrix) {
-        var zeroRows = new HashSet<Integer>();
-        var zeroCols = new HashSet<Integer>();
+        printMatrix(matrix, 2);
+        boolean clearFirstCol = false;
         for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[i].length; j++) {
-                if (matrix[i][j] == 0) {
-                    zeroRows.add(i);
-                    zeroCols.add(j);
+            // some element in first column is zero
+            if(matrix[i][0] == 0) {
+                clearFirstCol = true;
+            }
+            
+            for(int j=1; j<matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][j] = -1; // not necessary just to avoid confusion
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                } 
+            }
+        }
+        
+        printMatrix(matrix, 2);
+        
+        for(int i=1; i<matrix.length; i++) {
+            for(int j=1; j<matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
         }
-        for(int i: zeroRows) {
-            for(int j=0; j<matrix[i].length; j++) matrix[i][j] = 0;
+        
+        printMatrix(matrix, 2);
+        
+        if(matrix[0][0] == 0 && clearFirstCol) {
+            for(int i=0; i<matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+            for(int j=0; j<matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        } else if(matrix[0][0] == 0 && !clearFirstCol) {
+            for(int j=0; j<matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        } else if(matrix[0][0] != 0 && clearFirstCol) {
+            for(int i=0; i<matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
         }
-        for(int j: zeroCols) {
-            for(int i=0; i<matrix.length; i++) matrix[i][j] = 0;
-        }
-    } 
+    }
     
-    public static void print(Object o, String sep) {
+    private void printMatrix(int[][] matrix, int space) {
+        for(int i=0; i<matrix.length; i++) {
+            System.out.print("[");
+            for(int j=0; j<matrix[0].length; j++) {
+                for(int x=0; x<Math.max(0,space-(""+matrix[i][j]).length()); x++) System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                if (j != matrix[0].length-1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("]");
+        }
+        System.out.println();
+    }
+    
+    private void print(Object o) {
+        System.out.println(o);
+    }
+    
+    private void print() {
+        System.out.println();
+    }
+    
+    private void print(Object o, String sep) {
         System.out.print(o + sep);
-    }   
-    public static void print(Object o) {
-        print(o, "\n");
     }
 }
