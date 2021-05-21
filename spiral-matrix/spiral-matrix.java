420c3a021fc6c1da6b6061fd0d14253e6f848352
Yash Rastogi <yashrastogi@gmail.com>
2021-05-21 16:02:13 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (5.33%) - LeetHub

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int r1=0, r2=matrix.length-1, c1=0, c2=matrix[0].length-1;
         var ret = new ArrayList<Integer>();
-        while (c1 <= c2 && r1 <= r2) {
-            for(int i=c1; i<=c2; i++) {
-                ret.add(matrix[r1][i]);
+        int c1=0, c2=matrix[0].length, r1=0, r2=matrix.length;
+        while(c1 < c2 && r1 < r2) {
+            for(int c=c1; c<c2; c++) {
+                // System.out.println("1 " + matrix[r1][c]);
+                ret.add(matrix[r1][c]);
             }
-            for(int j=r1+1; j<=r2; j++) {
-                ret.add(matrix[j][c2]);
+            for(int r=r1+1; r<r2; r++) {
+                // System.out.println("2 " + matrix[r][c2-1]);
+                ret.add(matrix[r][c2-1]);
             }
-            if (r1 < r2 && c1 < c2) {
-                for(int k=c2-1; k>c1; k--) {
-                    ret.add(matrix[r2][k]);
-                }
-                for(int l=r2; l>r1; l--) {
-                    ret.add(matrix[l][c1]);
-                }
+            if(c1 < c2-1 && r1 < r2-1){
+            for(int c=c2-1-1; c>=c1; c--) {
+                // System.out.println("3 " + matrix[r2-1][c]);
+                ret.add(matrix[r2-1][c]);
             }
-            c1++; r1++; r2--; c2--;
+            for(int r=r2-1-1; r>=r1+1; r--) {
+                // System.out.println("4 " + matrix[r][c1]);
+                ret.add(matrix[r][c1]);
+            }}
+            c1++; c2--;
+            r1++; r2--;
         }
         return ret;
     }
-    public static void print(Object o) {
-        print(o, "\n");
-    }
-    public static void print(Object o, String sep) {
-        System.out.print(o + sep);
-    } 
 }
