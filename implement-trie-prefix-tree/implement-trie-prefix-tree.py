1a847a456b56e3e9424a85b0960ade4f490ceff6
Yash Rastogi <yashrastogi@gmail.com>
2021-05-23 11:45:25 +0530

    Time: 328 ms (5.54%), Space: 35.6 MB (5.22%) - LeetHub

--- implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,65 +1,68 @@
 class TrieNode:
-  def __init__(self):
-    self.children = [None] * 26
-    self.isEndOfWord = False
+    def __init__(self):
+        self.children = [None for _ in range(26)]
+        self.isEndOfWord = False
+        
+    def __str__(self):
+        ret_str = ''
+        ret_str += '[TrieNode (C): '
+        for i, el in enumerate(self.children):
+            if el != None:
+                ret_str += str(i) + ' '
+        ret_str += ']\n'
+        return ret_str
 
 class Trie:
-  def _charNum(self, ch):
-    """
-    Returns 0-25 for a-z
-    """
-    return ord(ch) - ord('a')
 
-  def __init__(self):
-    """
-    Initialize your data structure here.
-    """
-    self.root = TrieNode()
-
-
-  def insert(self, word: str) -> None:
-    """
-    Inserts a word into the trie.
-    """
-    level = len(word)
-    curr = self.root
-    for i in range(level):
-      charNum = self._charNum(word[i])
-      if not curr.children[charNum]:
-        curr.children[charNum] = TrieNode()
-      curr = curr.children[charNum]
-    curr.isEndOfWord = True
-
-  def search(self, word: str) -> bool:
-    """
-    Returns if the word is in the trie.
-    """
-    level = len(word)
-    curr = self.root
-    for i in range(level):
-      charNum = self._charNum(word[i])
-      if not curr.children[charNum]:
-        return False
-      curr = curr.children[charNum]
-    if curr.isEndOfWord:
-      return True
-    return False
-
-
-  def startsWith(self, prefix: str) -> bool:
-    """
-    Returns if there is any word in the trie that starts with the given prefix.
-    """
-    level = len(prefix)
-    curr = self.root
-    for i in range(level):
-      charNum = self._charNum(prefix[i])
-      if not curr.children[charNum]:
-        return False
-      curr = curr.children[charNum]
-    return True
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
         
 
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        length = len(word)
+        node = self.root
+        
+        for i in range(length):
+            curr = self._idx(word[i])
+            if not node.children[curr]:
+                node.children[curr] = TrieNode()
+            node = node.children[curr]
+            
+        node.isEndOfWord = True
+            
+    def _idx(self, ch):
+        return ord(ch) - ord('a')
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        length = len(word)
+        node = self.root
+        for i in range(length):
+            curr = self._idx(word[i])
+            node = node.children[curr]
+            if not node: return False
+        return node.isEndOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        length = len(prefix)
+        node = self.root
+        for i in range(length):
+            curr = self._idx(prefix[i])
+            node = node.children[curr]
+            if not node: return False
+        return True
+        
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
