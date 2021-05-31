e61d4d18b37d02041218126f1a1f5c88cdb5bf64
Yash Rastogi <yashrastogi@gmail.com>
2021-05-31 19:59:05 +0530

    Time: 56 ms (87.75%), Space: 75.9 MB (11.29%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,35 +1,28 @@
+#define vi vector<int>
+#define vvi vector<vector<int>>
+
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum_nums = accumulate(nums.begin(), nums.end(), 0);
-        if (sum_nums % 2 != 0) return false;
-        int target_sum = sum_nums / 2;
-        int l1 = nums.size() + 1, l2 = target_sum + 1;
-        vector<vector<bool>> dp = vector<vector<bool>>(l1, vector<bool>(l2, 0));
-        for(int i=0; i<l1; i++) {
-            for(int j=0; j<l2; j++) {
-                if(i == 0) 
-                    dp[i][j] = 0;
-                if(j == 0)
-                    dp[i][j] = 1;
-            }
-        }
-        
-        for(int i=1; i<l1; i++) {
-            for(int j=1; j<l2; j++) {
-                /*
-                    Choices (i is nums to consider, j is varing amount):
-                    1. take current idx, move next idx
-                    2. don't take current idx, move next idx --- default fallback
-                */
-                if(int t = j - nums[i-1]; t >= 0) {
-                    dp[i][j] = dp[i-1][t] || dp[i-1][j];
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
-                
-            }
-        }
-        return dp[nums.size()][target_sum];
+        int targetSum = accumulate(nums.begin(), nums.end(), 0);
+        if (targetSum % 2 != 0) return false;
+        targetSum /= 2;
+        int l1 = nums.size() + 1, l2 = targetSum + 1; 
+        vvi memo = vvi(l1, vi(l2, -1));
+        return recursion(nums, nums.size(), targetSum, memo);
+    }
+    
+    bool recursion(vi& nums, int idx, int rem, vvi& memo) {
+        if(idx < 1) return false;
+        if(rem < 0) return false;
+        if(rem == 0) return true;
+        if(int t = memo[idx][rem]; t != -1) return t;
+        /*
+            Choices:
+            1. Take current idx, move to next idx
+            2. Don't take current idx, move to next idx
+        */
+        return memo[idx][rem] = recursion(nums, idx - 1, rem - nums[idx - 1], memo) ||
+               recursion(nums, idx - 1, rem, memo);
     }
 };
