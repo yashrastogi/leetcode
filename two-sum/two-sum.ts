bbc30733f113f12d31aacf4ce631ddc12cbe3993
Yash Rastogi <yashrastogi@gmail.com>
2021-09-11 13:09:05 +0530

    Time: 125 ms (34.58%), Space: 40.3 MB (98.50%) - LeetHub

---
+++ two-sum/two-sum.ts
@@ -0,0 +1,8 @@
+function twoSum(nums: number[], target: number): number[] {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in map) return [map[target - nums[i]], i];
+    map[nums[i]] = i;
+  }
+  return null;
+}
