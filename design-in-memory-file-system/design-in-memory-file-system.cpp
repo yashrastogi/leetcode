f816deece26a4efbf5f41a90e485332973a615be
Yash Rastogi <yashrastogi@gmail.com>
2021-09-26 17:55:41 +0530

    Time: 20 ms (41.97%), Space: 15.5 MB (37.59%) - LeetHub

--- design-in-memory-file-system/design-in-memory-file-system.cpp
+++ design-in-memory-file-system/design-in-memory-file-system.cpp
@@ -16,20 +16,9 @@ class FileSystem {
     FileNode *root;
 
    public:
-    FileSystem() {
-        cout << __func__ << " entered!" << '\n';
-        root = new FileNode("root");
-        // auto testNode1 = new FileNode("testnodea");
-        // auto testNode2 = new FileNode("testnodeb", false);
-        // testNode2->content = "testContent";
-        // testNode1->children.push_back(testNode2);
-        // root->children.push_back(testNode1);
-        cout << __func__ << " finished!" << '\n';
-    }
+    FileSystem() { root = new FileNode("root"); }
 
     vector<string> ls(string path) {
-        cout << __func__ << " entered!" << '\n';
-
         auto curr = root;
         auto pathSplit = stringSplit(path, '/');
         vector<string> ret;
@@ -44,15 +33,11 @@ class FileSystem {
             for (auto child : curr->children) ret.push_back(child->name);
         else
             ret.push_back(curr->name);
-
-        cout << __func__ << " finished!" << '\n';
         sort(ret.begin(), ret.end());
         return ret;
     }
 
     void mkdir(string path) {
-        cout << __func__ << " entered!" << '\n';
-
         auto curr = root;
         auto pathSplit = stringSplit(path, '/');
         int depth = 0;
@@ -72,50 +57,34 @@ class FileSystem {
                 depth += 1;
             }
         }
-
-        cout << __func__ << " finished!" << '\n';
     }
 
     string readContentFromFile(string filePath) {
-        cout << __func__ << " entered!" << '\n';
-
         auto curr = root;
         int depth = 0;
         auto pathSplit = stringSplit(filePath, '/');
         while (depth < pathSplit.size()) {
-            cout << curr->name << ": ";
             for (auto child : curr->children) {
-                cout << child->name << ' ';
-                cout << "trying to find: " << pathSplit[depth] << " ";
                 if (child->name == pathSplit[depth]) {
                     curr = child;
                     depth += 1;
                 }
             }
-            cout << " endCurrName: " << curr->name << '\n';
         }
-        cout << __func__ << " finished!" << '\n';
         return curr->content;
     }
 
     void addContentToFile(string filePath, string content) {
-        cout << __func__ << " entered!" << '\n';
-
         auto curr = root;
         int depth = 0;
         auto pathSplit = stringSplit(filePath, '/');
         // maxDepth - 2 as get parent dir add child file
-        cout << filePath << ' ' << content << '\n';
         while (depth + 1 < pathSplit.size())
             for (auto child : curr->children)
                 if (child->name == pathSplit[depth]) {
                     curr = child;
                     depth += 1;
                 }
-
-        cout << "curr->name: " << curr->name
-             << " pathSplit[d]: " << pathSplit[depth] << '\n';
-
         FileNode *fileIfFound = nullptr;
         for (auto child : curr->children)
             if (child->name == pathSplit[depth]) fileIfFound = child;
@@ -127,11 +96,9 @@ class FileSystem {
             tempNode->content = content;
             curr->children.push_back(tempNode);
         }
-        cout << __func__ << " finished!" << '\n';
     }
 
     vector<string> stringSplit(string s, char delim = ' ') {
-        cout << __func__ << " entered!" << '\n';
         if (s == "/") return vector<string>();
         vector<string> ret;
         int start, end;
@@ -140,8 +107,6 @@ class FileSystem {
             ret.push_back(s.substr(start, end - start));
         ret.push_back(s.substr(start, end - start));
         if (ret.size() > 0) ret.erase(ret.begin());
-
-        cout << __func__ << " finished!" << '\n';
         return ret;
     }
 };
