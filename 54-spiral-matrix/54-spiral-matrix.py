5c00b4f8fa86098ab88b0d033b8254ee123ccf29
Yash Rastogi <yashrastogi@gmail.com>
2022-06-22 23:28:16 +0530

    Time: 59 ms (15.93%), Space: 13.7 MB (99.03%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -6,20 +6,16 @@ class Solution:
         while len(res) < len(matrix) * len(matrix[0]):
             for i in range(L_B, R_B):
                 res.append(matrix[U_B][i])
-            # print(res,1)
             for i in range(U_B + 1, D_B):
                 res.append(matrix[i][R_B-1])
-            # print(res,2)
             if len(res) >= len(matrix) * len(matrix[0]):
                 break
             for i in reversed(range(L_B, R_B-1)):
                 res.append(matrix[D_B-1][i])
-            # print(res,3)
             for i in reversed(range(U_B+1, D_B-1)):
                 res.append(matrix[i][L_B])
             U_B += 1
             R_B -= 1
             L_B += 1
             D_B -= 1
-            # print(res)
         return res
