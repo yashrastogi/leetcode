086b6a4ced031f64551dcb18bc61cb64083dad35
Yash Rastogi <yashrastogi@gmail.com>
2023-09-24 13:39:29 +0530

    Time: 200 ms (39.6%), Space: 25 MB (26.06%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,12 +7,6 @@ class Solution:
             prefix.append(prefix[i  - 1]* nums[i - 1])
         for j in reversed(range(len(nums)-1)):
             suffix.append(suffix[-1] * nums[j + 1])
-        print(prefix, suffix)
-        # temp_prefix = 1
         for i, num in enumerate(nums):
-            # temp_suffix = 1
-            # for j in range(i+1, len(nums)):
-                # temp_suffix *= nums[j]
-            ret.append(prefix[i] * suffix[-i-1])
-            
+            ret.append(prefix[i] * suffix[-i-1])    
         return ret
