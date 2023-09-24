f9f4e213663d2c325282223ac26784483a4344ef
Yash Rastogi <yashrastogi@gmail.com>
2023-09-24 14:52:09 +0530

    Time: 99 ms (73.96%), Space: 16.3 MB (73.95%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,8 +1,11 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_set = set()
+        col_set = set()
+        box_set = set()
         for i in range(9):
-            row_set = set()
-            col_set = set()
+            row_set.clear()
+            col_set.clear()
             for j in range(9):
                 if board[i][j] not in row_set:
                     row_set.add(board[i][j])
@@ -13,7 +16,7 @@ class Solution:
                 elif board[j][i] != '.':
                     return False
                 if i % 3 == 0 and j % 3 == 0:
-                    box_set = set()
+                    box_set.clear()
                     for k in range(i, i + 3):
                         for l in range(j, j + 3):
                             if board[k][l] not in box_set:
