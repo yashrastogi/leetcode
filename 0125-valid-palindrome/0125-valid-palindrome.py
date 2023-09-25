febe89819fad089cdf68898780a236ed50854c06
Yash Rastogi <yashrastogi@gmail.com>
2023-09-25 14:26:46 +0530

    Time: 84 ms (5.12%), Space: 17.1 MB (59.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         news = ''
-        s = s.upper()
+        s = s.lower()
         for c in s:
-            if ord(c) in range(65, 91) or ord(c) in range(48,58):
+            if ord(c) in range(ord('a'), ord('z')+1) or ord(c) in range(ord('0'),ord('9') + 1):
                 news += c
-        print(news)
         i, j = 0, len(news) - 1
         while i < j:
             if news[i] != news[j]:
