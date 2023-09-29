8196694c044733db150bc52234c0c597ced1da63
Yash Rastogi <yashrastogi@gmail.com>
2023-09-29 11:19:55 +0530

    Time: 127 ms (26.73%), Space: 18.1 MB (81.82%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,10 +5,9 @@ class Solution:
             left_max.append(max(left_max[-1], height[i]))
         right_max = [height[-1]]
         for i in reversed(range(len(height) - 1)):
-            right_max.insert(0, max(right_max[0], height[i]))
-        # print(left_max)
-        # print(right_max)
+            right_max.append(max(right_max[-1], height[i]))
+        right_max.reverse()
         water_trapped = 0
         for i in range(len(height)):
-            water_trapped += max(0, min(left_max[i], right_max[i]) - height[i])
+            water_trapped += min(left_max[i], right_max[i]) - height[i]
         return water_trapped
