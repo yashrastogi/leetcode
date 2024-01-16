8416975b7a9fd1926b60bd2045301035a25eb0e8
Yash Rastogi <yashrastogi@gmail.com>
2024-01-16 14:32:44 +0530

    Time: 39 ms (97.89%), Space: 19.7 MB (8.19%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -4,10 +4,8 @@ class Solution:
         for i in range(len(s)):
             if s[i] in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
                 vowel_locations.append(i)
-        print(vowel_locations)
         sc = list(s)
         for i in range(len(vowel_locations) // 2):
-            print(i)
             num1 = vowel_locations[i]
             num2 = vowel_locations[-i-1]
             temp = sc[num1]
