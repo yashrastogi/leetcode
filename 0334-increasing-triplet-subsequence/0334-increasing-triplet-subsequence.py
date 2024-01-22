f011d040cb2bb3a7c888e80037732cb7f2b4d594
Yash Rastogi <yashrastogi@gmail.com>
2024-01-22 14:26:12 +0530

    Time: 782 ms (93.07%), Space: 32.6 MB (69.58%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,11 +1,11 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         min1 = min2 = float("inf")
-        for i in range(len(nums)):
-            if min1 >= nums[i]:
-                min1 = nums[i]
-            elif min2 >= nums[i]:
-                min2 = nums[i]
+        for num in nums:
+            if min1 >= num:
+                min1 = num
+            elif min2 >= num:
+                min2 = num
             else:
                 # min2 will only be set when there exists a number min1 less than min2 existing before it
                 # so printing it in seq min1 min2 curr might not yield correct output even though the algorithm
