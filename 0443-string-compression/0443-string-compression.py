18f1457ab7ccfff8fb5759265b786cb4e82e5502
Yash Rastogi <yashrastogi@gmail.com>
2024-01-22 15:09:30 +0530

    Time: 52 ms (95.3%), Space: 16.7 MB (60.46%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,33 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        curr_char = ""
+        count = 0
+        writer_ptr = 0
+        for i, char in enumerate(chars):
+            if curr_char == "":
+                curr_char = char
+            if curr_char != char:
+                chars[writer_ptr] = curr_char
+                writer_ptr += 1
+                if count > 9:
+                    str_count = str(count)
+                    for c in str_count:
+                        chars[writer_ptr] = c
+                        writer_ptr += 1
+                elif count > 1:
+                    chars[writer_ptr] = str(count)
+                    writer_ptr += 1
+                curr_char = char
+                count = 0
+            count += 1
+        chars[writer_ptr] = curr_char
+        writer_ptr += 1
+        if count > 9:
+            str_count = str(count)
+            for c in str_count:
+                chars[writer_ptr] = c
+                writer_ptr += 1
+        elif count > 1:
+            chars[writer_ptr] = str(count)
+            writer_ptr += 1
+        return writer_ptr
