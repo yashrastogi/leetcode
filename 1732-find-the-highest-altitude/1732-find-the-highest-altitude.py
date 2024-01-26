a8ebfa1d5f9b3b4100bfa8c0c8bc51873527969b
Yash Rastogi <yashrastogi@gmail.com>
2024-01-26 16:54:02 +0530

    Time: 40 ms (60.63%), Space: 16.7 MB (55.86%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -4,5 +4,5 @@ class Solution:
         max_alt = 0
         for alt in gain:
             prefix_sum += alt
-            max_alt = max(max_alt, prefix_sum)
-        return max(0, max_alt)
+            max_alt = max(max_alt, prefix_sum, 0)
+        return max_alt
