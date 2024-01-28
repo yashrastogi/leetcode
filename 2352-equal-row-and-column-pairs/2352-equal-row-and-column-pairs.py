4edf3f6e9a861eae146298e8844a0dfa34606ead
Yash Rastogi <yashrastogi@gmail.com>
2024-01-28 13:52:14 +0530

    Time: 394 ms (82.1%), Space: 21.1 MB (97.75%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -4,14 +4,15 @@ class Solution:
         rows = Counter()
         for i in range(len(grid)):
             cur_col = []
-            cols[str(grid[i])] += 1
+            cols[tuple(grid[i])] += 1
         for i in range(len(grid[0])):
             cur_row = []
             for j in range(len(grid)):
                 cur_row += [grid[j][i]]
-            rows[str(cur_row)] += 1
+            rows[tuple(cur_row)] += 1
         ret = 0
         for key in rows:
             if key in cols:
                 ret += cols[key] * rows[key]
+        # print(rows, cols)
         return ret
