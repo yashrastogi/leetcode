b439b8a9cf8b2791ea635766f14953f183c7e623
Yash Rastogi <yashrastogi@gmail.com>
2024-01-30 14:08:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,20 +1,40 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        def repeat(string, i):
-            ret = ''
+        def repeatRecursion(string, i):
+            ret = ""
             reps = 0
             while i < len(s):
                 if string[i].isnumeric():
                     reps = reps * 10 + int(string[i])
-                elif string[i] == '[':
-                    substr, i = repeat(string, i + 1)
+                elif string[i] == "[":
+                    substr, i = repeatRecursion(string, i + 1)
                     ret += substr * reps
                     reps = 0
-                elif string[i] == ']':
+                elif string[i] == "]":
                     return ret, i
                 else:
                     ret += string[i]
                 i += 1
             return ret, i
-        
-        return repeat(s, 0)[0]
+
+        def repeatStack(string):
+            i = 0
+            ret = ""
+            reps = 0
+            stack = []
+            substr = None
+            while i < len(string):
+                print(stacl)
+                if string[i].isnumeric():
+                    reps = reps * 10 + int(string[i])
+                elif string[i] == "[":
+                    stack.append([i + 1, reps])
+                    reps = 0
+                elif string[i] == "]":
+                    popped = stack.pop()
+                    stack[-1].append(string[popped[0]:i] * popped[1])
+
+                i += 1
+            return ret
+
+        return repeatStack(s)
