7f6fbfd7bab23860548e3c28daec318c410718b3
Yash Rastogi <yashrastogi@gmail.com>
2024-01-30 16:22:14 +0530

    Time: 190 ms (86.34%), Space: 22.3 MB (58.71%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -1,16 +1,13 @@
 class RecentCounter:
-
     def __init__(self):
-        self.storage = []        
+        self.storage = []
 
     def ping(self, t: int) -> int:
-        self.storage.insert(0, t)
-        low = max(1, t - 3000)
-        counter = 0
-        for i in range(min(len(self.storage), 3001)):
-            if self.storage[i] >= low:
-                counter += 1
-        return counter
+        self.storage.append(t)
+        low = t - 3000
+        while self.storage[0] < low:
+            self.storage.pop(0)
+        return len(self.storage)
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
