16b71f633ebc39c5d74c5b7e556c0cc456445e1b
Yash Rastogi <yashrastogi@gmail.com>
2024-02-06 16:10:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -1,12 +1,22 @@
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        dict_depth_sums = defaultdict(int)
+        max_sum = -float('inf')
+        max_sum_depth = 0
+        curr_sum = 0
+        curr_depth = 0
         q = [(root, 1)]
         while q:
             curr, depth = q.pop(0)
-            dict_depth_sums[depth] += curr.val
+            if depth == curr_depth:
+                curr_sum += curr.val
+            else:
+                if curr_sum > max_sum:
+                    max_sum = curr_sum
+                    max_sum_depth = curr_depth
+                curr_depth = depth
+                curr_sum = curr.val
             if curr.left:
                 q.append((curr.left, depth + 1))
             if curr.right:
                 q.append((curr.right, depth + 1))
-        return max(dict_depth_sums, key=lambda depth: (dict_depth_sums[depth], -depth))
+        return max_sum_depth
