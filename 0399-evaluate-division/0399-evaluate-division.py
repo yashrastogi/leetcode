18f6e8a49e6f194fdbf25a67f8b6c9e05a319c4f
Yash Rastogi <yashrastogi@gmail.com>
2024-02-23 16:35:44 +0530

    Time: 42 ms (27.78%), Space: 16.7 MB (74.77%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -4,19 +4,16 @@ class Solution:
 
         def dfs(curr, dest):
             visited.add(curr)
-            if curr == dest: return 1
+            if curr == dest:
+                return 1
             ret_val = -1
             for d in graph[curr]:
                 if dest == d[0]:
-                    # print(curr, "->", d[0], "(", d[1], ")", end="\t")
-                    # print("END")
                     return d[1]
                 elif d[0] not in visited:
-                    # print(curr, "->", d[0], "(", d[1], ")", end="\t")
                     test = dfs(d[0], dest)
                     if test != -1:
                         ret_val = d[1] * test
-            # print("END")
             return ret_val
 
         graph = defaultdict(list)
@@ -24,13 +21,10 @@ class Solution:
             graph[eq[0]] += [(eq[1], values[i])]
             graph[eq[1]] += [(eq[0], 1 / values[i])]
         res = []
-        # import pprint; pprint.pprint(graph)
         for q in queries:
             if q[0] not in graph or q[1] not in graph:
                 res.append(-1)
             else:
-                # print(q)
                 res.append(dfs(q[0], q[1]))
                 visited.clear()
-                # print("\n" * 3)
         return res
