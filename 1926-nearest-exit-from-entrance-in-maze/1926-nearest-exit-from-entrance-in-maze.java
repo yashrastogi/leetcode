c70afc4c0ce4fd06cee6c5f3bb734526bb938439
Yash Rastogi <yashrastogi@gmail.com>
2024-02-25 16:54:28 +0530

    Time: 48 ms (9.19%), Space: 46.6 MB (6.3%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -2,29 +2,26 @@ class Solution {
     public int nearestExit(char[][] maze, int[] ent) {
         Set<String> visited = new HashSet<>();
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{ent[0], ent[1], 0});
+        q.add(new int[] { ent[0], ent[1], 0 });
 
         while (!q.isEmpty()) {
             int[] curr = q.poll();
             int x = curr[0], y = curr[1], level = curr[2];
             String key = x + "," + y;
 
-            if (visited.contains(key)) {
+            if (visited.contains(key))
                 continue;
-            }
 
             visited.add(key);
 
-            if (!isArrayEqual(curr, ent) && (x == 0 || x == maze.length - 1 || y == 0 || y == maze[0].length - 1)) {
+            if (!isArrayEqual(curr, ent) && (x == 0 || x == maze.length - 1 || y == 0 || y == maze[0].length - 1))
                 return level;
-            }
 
-            int[][] directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+            int[][] directions = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
             for (int[] dir : directions) {
                 int nx = x + dir[0], ny = y + dir[1];
-                if (0 <= nx && nx < maze.length && 0 <= ny && ny < maze[0].length && maze[nx][ny] == '.') {
-                    q.add(new int[]{nx, ny, level + 1});
-                }
+                if (0 <= nx && nx < maze.length && 0 <= ny && ny < maze[0].length && maze[nx][ny] == '.')
+                    q.add(new int[] { nx, ny, level + 1 });
             }
         }
 
