c7a925cdb40627e3400963129ec9cf984ac51c86
Yash Rastogi <yashrastogi@gmail.com>
2024-02-25 17:57:53 +0530

    Time: 62 ms (15.86%), Space: 16.5 MB (80.54%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -15,7 +15,7 @@ class Solution:
             max_level = max(level, max_level)
             visited.add(curr)
             if len(visited) == len(grid) * len(grid[0]):
-                return max_level
+                break
             for x, y in ((0, 1), (1, 0), (0, -1), (-1, 0)):
                 cd = (curr[0] + x, curr[1] + y)
                 if (
@@ -26,4 +26,4 @@ class Solution:
                     q.append(((cd, level + 1)))
         if len(visited) != len(grid) * len(grid[0]):
             return -1
-        return 0
+        return max_level
