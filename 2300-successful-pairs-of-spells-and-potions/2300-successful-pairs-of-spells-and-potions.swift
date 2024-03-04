9c86f695bdfb35bd589e8113a7443aff8b4be0ae
Yash Rastogi <yashrastogi@gmail.com>
2024-03-04 16:06:35 +0530

    Time: 2072 ms (6.98%), Space: 27.3 MB (9.3%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func successfulPairs(_ spells: [Int], _ potions: [Int], _ success: Int) -> [Int] {
+        var pairs = [Int]()
+        var potions = potions.sorted()
+        for spell in spells {
+            var count = 0
+            let searchVal = Int(ceil(Double(success) / Double(spell)))
+            if potions[potions.count - 1] < searchVal {
+                pairs.append(0)
+                continue
+            }
+            let closeIdx = binarySearch(0, potions.count - 1, searchVal, potions)
+            count += potions.count - closeIdx
+            pairs.append(count)
+        }
+        return pairs
+    }
+
+    func binarySearch(_ lo: Int, _ hi: Int, _ search: Int, _ array: [Int]) -> Int {
+        if lo > hi {
+            return hi + 1
+        }
+        var mid = (lo + hi) / 2
+        if array[mid] > search {
+            return binarySearch(lo, mid - 1, search, array)
+        } else if array[mid] == search {
+            while mid - 1 >= 0 && array[mid - 1] == search {
+                mid -= 1
+            }
+            return mid
+        } else {
+            return binarySearch(mid + 1, hi, search, array)
+        }
+    }
+}
