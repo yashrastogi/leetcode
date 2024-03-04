c59e096910d006b4739afada5f051be19ff7eceb
Yash Rastogi <yashrastogi@gmail.com>
2024-03-04 16:32:55 +0530

    Time: 600 ms (95.35%), Space: 27.3 MB (16.28%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.swift
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.swift
@@ -1,32 +1,31 @@
 class Solution {
     func successfulPairs(_ spells: [Int], _ potions: [Int], _ success: Int) -> [Int] {
-        var pairs = [Int]()
-        let potions = potions.sorted()
-        for spell in spells {
-            var count = 0
-            let searchVal = Int(ceil(Double(success) / Double(spell)))
-            if potions[potions.count - 1] < searchVal {
-                pairs.append(0)
-                continue
-            }
-            let closeIdx = binarySearch(searchVal, potions)
-            count += potions.count - closeIdx
-            pairs.append(count)
+        let sortedPotions = potions.sorted()
+        let searchValues = spells.map { spell in
+            return Int(ceil(Double(success) / Double(spell)))
+        }
+        var pairs = [Int](repeating: 0, count: spells.count)
+        
+        for (index, searchVal) in searchValues.enumerated() {
+            let count = binarySearchCount(sortedPotions, searchVal)
+            pairs[index] = count
         }
         return pairs
     }
 
-    func binarySearch(_ search: Int, _ array: [Int]) -> Int {
-        var lo = 0
-        var hi = array.count - 1
-        while lo <= hi {
-            let mid = lo + (hi - lo) / 2
+    func binarySearchCount(_ array: [Int], _ search: Int) -> Int {
+        guard !array.isEmpty else { return 0 }
+        
+        var low = 0
+        var high = array.count - 1
+        while low <= high {
+            let mid = low + (high - low) / 2
             if array[mid] < search {
-                lo = mid + 1
+                low = mid + 1
             } else {
-                hi = mid - 1
+                high = mid - 1
             }
         }
-        return lo
+        return array.count - low
     }
 }
