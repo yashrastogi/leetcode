5d2613e10b2375c6a965d13326cb62d5ce26d4a5
Yash Rastogi <yashrastogi@gmail.com>
2024-03-05 17:58:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.swift
+++ 0162-find-peak-element/0162-find-peak-element.swift
@@ -1,9 +1,9 @@
 class Solution {
     func findPeakElement(_ nums: [Int]) -> Int {
-        if nums.count == 1 {
-            return 0
-        } else if nums.count == 2 {
-            return nums[1] > nums[0] ? 1 : 0
+        switch nums.count {
+        case 1: return 0
+        case 2: return nums[1] > nums[0] ? 1 : 2
+        default: break
         }
         for i in 0 ..< nums.count {
             if i == 0 {
