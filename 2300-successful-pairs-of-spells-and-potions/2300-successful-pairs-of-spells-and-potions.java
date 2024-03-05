3d3a2d8933250c985a505df44489f57ed8ecfe64
Yash Rastogi <yashrastogi@gmail.com>
2024-03-05 14:30:42 +0530

    Time: 42 ms (44.71%), Space: 56.9 MB (99.42%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int[] searchValues = new int[spells.length];
+        for (int i = 0; i < spells.length; i++) {
+            searchValues[i] = (int) Math.ceil((double) success / spells[i]);
+        }
+        int[] pairs = new int[spells.length];
+        for (int i = 0; i < spells.length; i++) {
+            int count = potions.length - binarySearch(potions, searchValues[i]);
+            pairs[i] = count;
+        }
+        return pairs;
+    }
+
+    private int binarySearch(int[] array, int search) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (array[mid] < search) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
