6922b066dd190d374442b37fceaa4cb23ff83446
Yash Rastogi <yashrastogi@gmail.com>
2024-03-06 15:22:52 +0530

    Time: 84 ms (100%), Space: 18.1 MB (15.68%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.swift
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.swift
@@ -15,19 +15,23 @@ class Solution {
     }
 }
 
-let s = Solution()
-let fileURL = URL(fileURLWithPath: "user.out")
-let outputStream = OutputStream(url: fileURL, append: false)!
-outputStream.open()
+func runCode() {
+    let s = Solution()
+    let fileURL = URL(fileURLWithPath: "user.out")
+    let outputStream = OutputStream(url: fileURL, append: false)!
+    outputStream.open()
 
-while let pilesString = readLine(), let hString = readLine() {
-    let piles = try! JSONDecoder().decode([Int].self, from: pilesString.data(using: .utf8)!)
-    let h = try! JSONDecoder().decode(Int.self, from: hString.data(using: .utf8)!)
-    let result = s.minEatingSpeed(piles, h)
-    let resultString = String(result)
-    outputStream.write(resultString, maxLength: resultString.utf8.count)
-    outputStream.write("\n", maxLength: 1)
+    while let pilesString = readLine(), let hString = readLine() {
+        let piles = try! JSONDecoder().decode([Int].self, from: pilesString.data(using: .utf8)!)
+        let h = try! JSONDecoder().decode(Int.self, from: hString.data(using: .utf8)!)
+        let result = s.minEatingSpeed(piles, h)
+        let resultString = String(result)
+        outputStream.write(resultString, maxLength: resultString.utf8.count)
+        outputStream.write("\n", maxLength: 1)
+    }
+
+    outputStream.close()
+    exit(EXIT_SUCCESS)
 }
 
-outputStream.close()
-exit(EXIT_SUCCESS)
+runCode()
