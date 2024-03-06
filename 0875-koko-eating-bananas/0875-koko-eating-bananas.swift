a9f18041f5c2d6ff771f3d90ff906833a89f6f45
Yash Rastogi <yashrastogi@gmail.com>
2024-03-06 15:03:41 +0530

    Time: 108 ms (45.41%), Space: 15.8 MB (25.41%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.swift
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.swift
@@ -3,12 +3,12 @@ class Solution {
         var lo = 1
         var hi = piles.max()!
         while lo <= hi {
-            let k = (lo + hi) / 2
+            let k = (hi - lo) / 2 + lo
             let kSum = piles.map { Int(ceil(Double($0) / Double(k))) }.reduce(0, +)
-            if kSum > h {
-                lo = k + 1
-            } else if kSum <= h {
+            if kSum <= h {
                 hi = k - 1
+            } else if kSum > h {
+                lo = k + 1
             }
         }
         return lo
