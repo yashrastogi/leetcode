41c0cc501f147931b17b8433d68c44608e8da5b6
Yash Rastogi <yashrastogi@gmail.com>
2024-03-06 15:17:49 +0530

    Time: 19 ms (98.78%), Space: 21.5 MB (15.29%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int minEatingSpeed(std::vector<int>& piles, int h) {
-        int lo = 1;
-        int hi = *std::max_element(piles.begin(), piles.end());
+        cout.tie(0);
+        cin.tie(0);
+        ios_base::sync_with_stdio(0);
+        int lo = 1, hi = *max_element(piles.begin(), piles.end());
         while (lo <= hi) {
             int k = (hi - lo) / 2 + lo;
             long kSum = 0;
-            for (int pile : piles) {
+            for (const auto& pile : piles) {
                 kSum += std::ceil(static_cast<double>(pile) / k);
             }
             if (kSum <= h) {
