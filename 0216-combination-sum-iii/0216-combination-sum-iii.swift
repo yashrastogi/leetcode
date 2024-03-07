712dcbc0951d9d6f2a2a586ecd711b7912ebdcf5
Yash Rastogi <yashrastogi@gmail.com>
2024-03-07 18:10:39 +0530

    Time: 269 ms (6.33%), Space: 15.9 MB (25.32%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func combinationSum3(_ k: Int, _ n: Int) -> [[Int]] {
+        func backtrack(_ times: Int, _ total: Int, _ curr: inout [Int], _ visited: inout Set<Int>) {
+            if times == k {
+                if curr.reduce(0, +) == n {
+                    if !res.contains(curr.sorted()) {
+                        res.insert(curr)
+                    }
+                }
+                return
+            }
+            for num in 1 ... 9 {
+                if !visited.contains(num) {
+                    curr.append(num)
+                    visited.insert(num)
+                    backtrack(times + 1, total + num, &curr, &visited)
+                    visited.remove(num)
+                    curr.removeLast()
+                }
+            }
+        }
+        var res: Set<[Int]> = []
+        var tempCurr: [Int] = []
+        var visited: Set<Int> = []
+        backtrack(0, 0, &tempCurr, &visited)
+        return Array(res)
+    }
+}
