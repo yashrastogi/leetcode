fbb8316fd3b0b07f924ff4f8a70ee1dc5bebf583
Yash Rastogi <yashrastogi@gmail.com>
2024-03-08 21:42:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.swift
+++ 0198-house-robber/0198-house-robber.swift
@@ -1,5 +1,13 @@
 class Solution {
-    func rob(_ nums: [Int]) -> Int {
+    func robIterative(_ nums: [Int]) -> Int {
+        var dp = nums
+        for i in stride(from: 2, to: nums.count, by: 1) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        }
+        return dp.last!
+    }
+
+    func robRecursive(_ nums: [Int]) -> Int {
         var memo = Array(repeating: -1, count: nums.count)
         func recursion(_ i: Int) -> Int {
             guard i < nums.count else { return 0 }
@@ -9,5 +17,9 @@ class Solution {
             return memo[i]
         }
         return recursion(0)
+    }
+
+    func rob(_ nums: [Int]) -> Int {
+        return robIterative(nums)
     }
 }
