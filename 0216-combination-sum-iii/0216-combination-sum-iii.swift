144f5f6a3732ee82c470dd2c8916c4e0cd6301dc
Yash Rastogi <yashrastogi@gmail.com>
2024-03-08 13:58:02 +0530

    Time: 4 ms (63.53%), Space: 15.5 MB (75.29%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.swift
+++ 0216-combination-sum-iii/0216-combination-sum-iii.swift
@@ -1,7 +1,7 @@
 class Solution {
     func combinationSum3(_ k: Int, _ n: Int) -> [[Int]] {
-        func backtrack(_ times: Int, _ total: Int, _ curr: inout [Int]) {
-            if times == k {
+        func backtrack(_ total: Int, _ curr: inout [Int]) {
+            if curr.count == k {
                 if total == n {
                     res.append(curr)
                 }
@@ -11,14 +11,14 @@ class Solution {
             }
             for num in (curr.last ?? 0) + 1 ..< 10 {
                 curr.append(num)
-                backtrack(times + 1, total + num, &curr)
+                backtrack(total + num, &curr)
                 curr.removeLast()
             }
         }
 
         var res: [[Int]] = []
         var tempCurr: [Int] = []
-        backtrack(0, 0, &tempCurr)
+        backtrack(0, &tempCurr)
         return res
     }
 }
