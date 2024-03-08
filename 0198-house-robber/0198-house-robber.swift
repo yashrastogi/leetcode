da87577806653a589a8e84c860f28c97a7c572ad
Yash Rastogi <yashrastogi@gmail.com>
2024-03-08 21:53:49 +0530

    Time: 2 ms (76.08%), Space: 15.3 MB (66.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.swift
+++ 0198-house-robber/0198-house-robber.swift
@@ -1,10 +1,11 @@
 class Solution {
     func robIterative(_ nums: [Int]) -> Int {
-        var dp = nums
-        for i in stride(from: 2, to: nums.count, by: 1) {
+        guard nums.count > 1 else { return nums[0] }
+        var dp = nums; dp[1] = max(nums[0], nums[1])
+        for i in stride(from: 2, to: dp.count, by: 1) {
             dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
         }
-        return dp.last!
+        return max(dp[dp.count - 1], dp[dp.count - 2])
     }
 
     func robRecursive(_ nums: [Int]) -> Int {
