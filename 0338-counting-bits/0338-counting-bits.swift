175f2c7f02dbc64a05d8f3d5832d7789b9525f92
Yash Rastogi <yashrastogi@gmail.com>
2024-03-18 12:39:14 +0530

    Time: 35 ms (83.44%), Space: 20.7 MB (71.17%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.swift
+++ 0338-counting-bits/0338-counting-bits.swift
@@ -1,7 +1,7 @@
 class Solution {
     func countBits(_ n: Int) -> [Int] {
         var ans = Array(repeating: 0, count: n + 1)
-        for i in 0 ... n {
+        for i in 1 ..< n + 1 {
             ans[i] = ans[i/2] + i % 2
         }
         return ans
