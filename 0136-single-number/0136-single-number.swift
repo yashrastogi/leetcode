389564661e6e661448b60e7605353c4d8bf78439
Yash Rastogi <yashrastogi@gmail.com>
2024-03-18 14:18:51 +0530

    Time: 45 ms (76.89%), Space: 15.4 MB (98.86%) - LeetHub

--- 0136-single-number/0136-single-number.swift
+++ 0136-single-number/0136-single-number.swift
@@ -1,9 +1,5 @@
 class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
-        var val = nums[0]
-        for i in 1 ..< nums.count {
-            val ^= nums[i]
-        }
-        return val
+        return nums.reduce(0, ^)
     }
 }
