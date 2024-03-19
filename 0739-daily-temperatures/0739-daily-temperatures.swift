5e31408ea69a410ead900a8b8e11309863636ccc
Yash Rastogi <yashrastogi@gmail.com>
2024-03-19 16:41:09 +0530

    Time: 544 ms (95.48%), Space: 26.5 MB (44.19%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.swift
+++ 0739-daily-temperatures/0739-daily-temperatures.swift
@@ -1,13 +1,13 @@
 class Solution {
     func dailyTemperatures(_ temps: [Int]) -> [Int] {
+        var stack = [(Int, Int)]()
         var answer = Array(repeating: 0, count: temps.count)
-        for i in 0 ..< temps.count {
-            for j in i ..< temps.count {
-                if temps[j] > temps[i] {
-                    answer[i] = j - i
-                    break
-                }
+        for (i, temp) in temps.enumerated() {
+            while !stack.isEmpty && stack.last!.1 < temp {
+                let el = stack.removeLast()
+                answer[el.0] = i - el.0
             }
+            stack.append((i, temp))
         }
         return answer
     }
