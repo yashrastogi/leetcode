539af8f629a24b6a1016fcc2c6915932633dea7a
Yash Rastogi <yashrastogi@gmail.com>
2024-03-20 17:17:38 +0530

    Time: 166 ms (56.73%), Space: 20.6 MB (95.10%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.swift
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.swift
@@ -1,12 +1,12 @@
 class Node: CustomStringConvertible {
     var root: Character
-    var children: [Character: Node] = [:]
+    var children: [Node] = []
     var wordEnd: Bool = false
-
+ 
     init(root: Character) {
         self.root = root
     }
-
+    
     var description: String {
         return "[root: \(root), children: \(children)]"
     }
@@ -14,40 +14,60 @@ class Node: CustomStringConvertible {
 
 class Trie {
     var rootNode = Node(root: "-")
-
+    
     func insert(_ word: String) {
         var curr = rootNode
-        var word = Array(word)
+        var word = Array(word)       
         var i = 0
         while i < word.count {
-            if let found = curr.children[word[i]] {
-                curr = found
-            } else {
+            var found = false
+            for c in curr.children {
+                if c.root == word[i] {
+                    curr = c
+                    found = true
+                    break
+                }
+            }
+            if !found {
                 let newNode = Node(root: word[i])
-                curr.children[word[i]] = newNode
+                curr.children.append(newNode)
                 curr = newNode
             }
             i += 1
         }
         curr.wordEnd = true
     }
-
-    func search(_ word: String, full: Bool? = true) -> Bool {
+    
+    func search(_ word: String, full: Bool? = true)  -> Bool {
         var curr = rootNode
         var word = Array(word)
         var i = 0
         while i < word.count {
-            if let found = curr.children[word[i]] {
-                curr = found
-            } else {
+            var found = false
+            for c in curr.children {
+                if c.root == word[i] {
+                    found = true
+                    curr = c
+                    break
+                }
+            }
+            if !found {
                 return false
             }
             i += 1
         }
         return full! ? curr.wordEnd : true
     }
-
+    
     func startsWith(_ prefix: String) -> Bool {
         return search(prefix, full: false)
     }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * let obj = Trie()
+ * obj.insert(word)
+ * let ret_2: Bool = obj.search(word)
+ * let ret_3: Bool = obj.startsWith(prefix)
+ */
