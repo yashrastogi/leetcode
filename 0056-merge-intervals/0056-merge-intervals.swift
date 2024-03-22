19744e139eb27563bd863d4f1ba8dfd1fb9389ea
Yash Rastogi <yashrastogi@gmail.com>
2024-03-22 16:46:09 +0530

    Time: 59 ms (83.78%), Space: 17.2 MB (72.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.swift
+++ 0056-merge-intervals/0056-merge-intervals.swift
@@ -1,19 +1,17 @@
 class Solution {
     func merge(_ intervals: [[Int]]) -> [[Int]] {
         var intervals = intervals.sorted { $0[0] < $1[0] }
-        var start = intervals[0][0], end = intervals[0][1]
+        var curr = intervals[0]
         var newIntervals = [[Int]]([])
         for interval in intervals {
-            if interval[0] <= end {
-                start = min(start, interval[0])
-                end = max(end, interval[1])
-            } else if interval[0] > end {
-                newIntervals.append([start, end])
-                start = interval[0]
-                end = interval[1]
+            if interval[0] <= curr[1] {
+                curr = [min(curr[0], interval[0]), max(curr[1], interval[1])]
+            } else {
+                newIntervals.append(curr)
+                curr = interval
             }
         }
-        newIntervals.append([start, end])
+        newIntervals.append(curr)
         return newIntervals
     }
 }
