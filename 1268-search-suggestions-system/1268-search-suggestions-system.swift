7424979583661632518cd62217bc24f358c7620d
Yash Rastogi <yashrastogi@gmail.com>
2024-03-22 15:00:00 +0530

    Time: 2051 ms (14.81%), Space: 61.4 MB (5.55%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.swift
+++ 1268-search-suggestions-system/1268-search-suggestions-system.swift
@@ -1,70 +1,83 @@
 class Solution {
-    let rootNode = Node(root: "-")
+    var rootNode: Node
     
-    func getWords(_ curr: Node) -> [[Character]] {
-        var ret = [[Character]]()
-        var q = [(curr, [Character]())]
-        while !q.isEmpty {
-            let (curr, wordArr) = q.removeFirst()
-            if curr.wordEnd {
-                ret.append(wordArr)
-            }
-            for c in curr.children.values {
-                q.append((c, wordArr + [c.root]))
-            }
-        }
-        return ret
+    init() {
+        rootNode = Node(root: "-")
     }
-
+    
     func suggestedProducts(_ products: [String], _ searchWord: String) -> [[String]] {
-        for p in products { insertTrie(p) }
+        for p in products {
+            insertTrie(p)
+        }
         var ret = [[String]]()
         var curr = rootNode
         var prefix = [Character]()
-        for (i, c) in Array(searchWord).enumerated() {
+        for (i, char) in searchWord.enumerated() {
+            prefix.append(char)
             var temp = [String]()
-            prefix.append(c)
-            if let found = curr.children[c] {
-                curr = found
+            if let child = curr.children[x(char)] {
+                curr = child
                 for suffix in getWords(curr) {
                     let builtWord = String(prefix + suffix)
                     temp.append(builtWord)
                 }
-                temp.sort()
-                temp = Array(temp[..<min(temp.count, 3)])
-                ret.append(temp)
             } else {
-                for j in i ..< searchWord.count {
+                var i = i
+                while i < searchWord.count {
                     ret.append([])
+                    i += 1
                 }
                 break
             }
+            ret.append(temp)
         }
+        return ret
+    }
+    
+    func getWords(_ curr: Node) -> [[Character]] {
+        func getWordsBacktrack(_ curr: Node, built: [Character]) {
+            if ret.count >= 3 {
+                return
+            }
+            if curr.wordEnd {
+                ret.append(built)
+            }
+            for c in curr.children {
+                if let c = c {
+                    getWordsBacktrack(c, built: built + [c.root])
+                }
+            }
+        }
+        var ret = [[Character]]()
+        getWordsBacktrack(curr, built: [])
         return ret
     }
     
     func insertTrie(_ p: String) {
         var curr = rootNode
-        for el in p {
-            curr = curr.children[el] ?? {
-                curr.children[el] = Node(root: el)
-                return curr.children[el]!
-            }()
+        for c in p {
+            let index = x(c)
+            if curr.children[index] == nil {
+                curr.children[index] = Node(root: c)
+            }
+            curr = curr.children[index]!
         }
         curr.wordEnd = true
     }
+    
+    func x(_ ch: Character) -> Int {
+        return Int(ch.asciiValue!) - Int(Character("a").asciiValue!)
+    }
 }
 
-class Node: CustomStringConvertible {
+class Node {
     var root: Character
-    var children: [Character: Node] = [:]
-    var wordEnd: Bool = false
-
+    var children: [Node?]
+    var wordEnd: Bool
+    
     init(root: Character) {
         self.root = root
-    }
-
-    var description: String {
-        return "[\(root): \(children)]"
+        self.children = Array(repeating: nil, count: 26)
+        self.wordEnd = false
     }
 }
