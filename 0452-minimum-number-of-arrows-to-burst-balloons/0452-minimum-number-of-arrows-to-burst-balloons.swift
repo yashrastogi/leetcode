5130729f7fe7f0bdcfd536da6d4ecb8bea4e5c41
Yash Rastogi <yashrastogi@gmail.com>
2024-03-23 14:45:59 +0530

    Time: 740 ms (69.95%), Space: 25.4 MB (44.60%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.swift
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.swift
@@ -1,9 +1,9 @@
 class Solution {
     func findMinArrowShots(_ points: [[Int]]) -> Int {
-        var points = points.sorted {$0[0] > $1[0]}
+        var points = points.sorted {$0[0] < $1[0]}
         var shootArrow = 0
         var curr = [Int.max, -Int.max]
-        while let interval = points.popLast() {
+        for interval in points {
             if interval[0] > curr[1] {
                 shootArrow += 1
                 curr = interval
