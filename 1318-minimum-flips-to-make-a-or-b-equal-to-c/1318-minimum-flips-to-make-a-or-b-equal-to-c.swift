54d4349286fa399f1bc806f5643373f36ad49cf1
Yash Rastogi <yashrastogi@gmail.com>
2024-03-23 19:49:26 +0530

    Time: 4 ms (58.33%), Space: 15.2 MB (37.50%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.swift
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.swift
@@ -1,26 +1,20 @@
 class Solution {
     func minFlips(_ a: Int, _ b: Int, _ c: Int) -> Int {
-        var flips = 0
         var a = a, b = b, c = c
-        
-        while c > 0 || a > 0 || b > 0 {
-            let bitA = a & 1
-            let bitB = b & 1
-            let bitC = c & 1
-            
-            if (bitA | bitB) != bitC {
+        var flips = 0
+        while a > 0 || b > 0 || c > 0 {
+            let bitA = a & 1, bitB = b & 1, bitC = c & 1
+            if bitA | bitB != bitC {
                 if bitC == 1 {
                     flips += 1
                 } else {
                     flips += (bitA == 1 ? 1 : 0) + (bitB == 1 ? 1 : 0)
                 }
             }
-            
             a >>= 1
             b >>= 1
             c >>= 1
         }
-        
         return flips
     }
 }
