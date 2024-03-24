1858cfe458fbd31ef0b0b6aaa2a2602a31f122b5
Yash Rastogi <yashrastogi@gmail.com>
2024-03-24 16:22:30 +0530

    Time: 13 ms (55.99%), Space: 15.4 MB (67.31%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.swift
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.swift
@@ -1,19 +1,22 @@
 class Solution {
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
-        var arr = [Int]()
-        matrix.forEach { m in arr += m }
-        return binarySearch(arr, target, 0, arr.count - 1)
+        return binarySearch(matrix, target, 0, (matrix.count * matrix[0].count) - 1)
     }
 
-    func binarySearch(_ arr: [Int], _ target: Int, _ lo: Int, _ hi: Int) -> Bool {
+    func indexMapper(_ i: Int, _ matrix: [[Int]]) -> [Int] {
+        return [i / matrix[0].count, i % matrix[0].count]
+    }
+
+    func binarySearch(_ matrix: [[Int]], _ target: Int, _ lo: Int, _ hi: Int) -> Bool {
         if lo > hi { return false }
         let mid = (hi - lo) / 2 + lo
-        if arr[mid] == target {
+        let ix = indexMapper(mid, matrix)
+        if matrix[ix[0]][ix[1]] == target {
             return true
-        } else if arr[mid] < target {
-            return binarySearch(arr, target, mid + 1, hi)
+        } else if matrix[ix[0]][ix[1]] < target {
+            return binarySearch(matrix, target, mid + 1, hi)
         } else {
-            return binarySearch(arr, target, lo, mid - 1)
+            return binarySearch(matrix, target, lo, mid - 1)
         }
     }
 }
