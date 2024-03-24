9b9cc965dc04d00b71cf0c58bb7d3d68b7798fcf
Yash Rastogi <yashrastogi@gmail.com>
2024-03-24 15:03:51 +0530

    Time: 571 ms (38.55%), Space: 31.8 MB (69.88%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.swift
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.swift
@@ -1,28 +1,41 @@
 class Solution {
     func partition(_ s: String) -> [[String]] {
-        var s = Array(s)
-        func backtrack(_ i: Int = 0) {
-            guard i < s.count else {
-                ret.append(temp)
-                return
-            }
-            for len in 0..<(s.count - i) {
-                let builtString = String(s[i...i + len])
-                if isPalindrome(builtString) {
-                    temp.append(builtString)
-                    backtrack(i + len + 1)
-                    temp.removeLast()
-                }
+        var result: [[String]] = []
+        var currentList: [String] = []
+        
+        backtrack(s, &result, &currentList, 0)
+        
+        return result
+    }
+    
+    func backtrack(_ s: String, _ result: inout [[String]], _ currentList: inout [String], _ start: Int) {
+        if start == s.count {
+            result.append(currentList)
+            return
+        }
+        
+        for end in start..<s.count {
+            if isPalindrome(s, start, end) {
+                let substring = String(s[s.index(s.startIndex, offsetBy: start)...s.index(s.startIndex, offsetBy: end)])
+                currentList.append(substring)
+                backtrack(s, &result, &currentList, end + 1)
+                currentList.removeLast()
             }
         }
-
-        var temp = [String]()
-        var ret = [[String]]()
-        backtrack()
-        return ret
     }
-
-    func isPalindrome(_ string: String) -> Bool {
-        return String(string.reversed()) == string
+    
+    func isPalindrome(_ s: String, _ left: Int, _ right: Int) -> Bool {
+        var leftIndex = left
+        var rightIndex = right
+        
+        while leftIndex < rightIndex {
+            if s[s.index(s.startIndex, offsetBy: leftIndex)] != s[s.index(s.startIndex, offsetBy: rightIndex)] {
+                return false
+            }
+            leftIndex += 1
+            rightIndex -= 1
+        }
+        
+        return true
     }
 }
