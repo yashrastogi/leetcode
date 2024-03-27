627e526f7e9c8ce4180b0fbcba81128b3d8a57d9
Yash Rastogi <yashrastogi@gmail.com>
2024-03-27 20:23:06 +0530

    Time: 24 ms (33.61%), Space: 15.9 MB (33.61%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.swift
+++ 0064-minimum-path-sum/0064-minimum-path-sum.swift
@@ -1,20 +1,21 @@
 class Solution {
     func minPathSumRecursive(_ grid: [[Int]]) -> Int {
-        var memo = [[Int]: Int]()
+        let maxLimit = 40001
+        var memo = Array(repeating: Array(repeating: -1, count: grid[0].count), count: grid.count)
 
         func dfs(_ i: Int, _ j: Int) -> Int {
             if i == grid.count - 1 && j == grid[0].count - 1 {
                 return grid[i][j]
             } else if i == grid.count || j == grid[0].count {
-                return 40001
+                return maxLimit
             }
-            if let found = memo[[i, j]] { return found }
-            var minSum = 40001
+            if memo[i][j] != -1 { return memo[i][j] }
+            var minSum = maxLimit
             for (d0, d1) in [(0, 1), (1, 0)] {
                 minSum = min(minSum, dfs(i + d0, j + d1) + grid[i][j])
             }
-            memo[[i, j]] = minSum
-            return memo[[i, j]]!
+            memo[i][j] = minSum
+            return memo[i][j]
         }
 
         return dfs(0, 0)
