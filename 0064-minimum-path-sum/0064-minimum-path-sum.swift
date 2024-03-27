4eadd5db7d7c0b26d0a3e6f7ed267cde23d9ca7c
Yash Rastogi <yashrastogi@gmail.com>
2024-03-27 22:25:35 +0530

    Time: 18 ms (92.62%), Space: 15.9 MB (41.8%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.swift
+++ 0064-minimum-path-sum/0064-minimum-path-sum.swift
@@ -1,4 +1,16 @@
 class Solution {
+    func minPathSumIterative(_ grid: [[Int]]) -> Int {
+        var dp = grid
+        for i in 1..<grid.count { dp[i][0] = dp[i - 1][0] + grid[i][0] }
+        for j in 1..<grid[0].count { dp[0][j] = dp[0][j - 1] + grid[0][j] }
+        for i in 1..<grid.count {
+            for j in 1..<grid[0].count {
+                dp[i][j] = min(dp[i][j - 1] + grid[i][j], dp[i - 1][j] + grid[i][j])
+            }
+        }
+        return dp[grid.count - 1][grid[0].count - 1]
+    }
+
     func minPathSumRecursive(_ grid: [[Int]]) -> Int {
         let maxLimit = 40001
         var memo = Array(repeating: Array(repeating: -1, count: grid[0].count), count: grid.count)
@@ -10,18 +22,13 @@ class Solution {
                 return maxLimit
             }
             if memo[i][j] != -1 { return memo[i][j] }
-            var minSum = maxLimit
-            for (d0, d1) in [(0, 1), (1, 0)] {
-                minSum = min(minSum, dfs(i + d0, j + d1) + grid[i][j])
-            }
-            memo[i][j] = minSum
+            memo[i][j] = min(dfs(i, j + 1) + grid[i][j], dfs(i + 1, j) + grid[i][j])
             return memo[i][j]
         }
-
         return dfs(0, 0)
     }
 
     func minPathSum(_ grid: [[Int]]) -> Int {
-        return minPathSumRecursive(grid)
+        return minPathSumIterative(grid)
     }
 }
