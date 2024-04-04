61b5e5e9b6579572d29c527e521a6948b2caacb9
Yash Rastogi <yashrastogi@gmail.com>
2024-04-04 16:16:36 +0530

    Time: 344 ms (31.58%), Space: 19.4 MB (7.02%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.swift
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.swift
@@ -1,12 +1,21 @@
 class Solution {
     func peakIndexInMountainArray(_ arr: [Int]) -> Int {
         var maxNum = -1, maxNumIndex = -1
-        for (i, num) in arr.enumerated() {
-            if num > maxNum {
-                maxNum = num
-                maxNumIndex = i
+        func binarySearch(_ lo: Int = 0, _ hi: Int = arr.count - 1) {
+            guard lo <= hi else { return }
+            let mid = (lo + hi) / 2
+
+            if arr[mid] > maxNum {
+                maxNum = arr[mid]
+                maxNumIndex = mid
+            }
+            if arr[mid] > arr[mid + 1] {
+                binarySearch(lo, mid - 1)
+            } else {
+                binarySearch(mid + 1, hi)
             }
         }
+        binarySearch()
         return maxNumIndex
     }
 }
