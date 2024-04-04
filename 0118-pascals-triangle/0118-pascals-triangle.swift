50cdd08c3b4efedc2e61cbd9253938b3799f7065
Yash Rastogi <yashrastogi@gmail.com>
2024-04-04 15:25:43 +0530

    Time: 3 ms (91.76%), Space: 16 MB (32.96%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.swift
+++ 0118-pascals-triangle/0118-pascals-triangle.swift
@@ -1,10 +1,10 @@
 class Solution {
     func generate(_ numRows: Int) -> [[Int]] {
         var rows = [[1]]
-        for i in 1 ..< numRows {
+        for i in stride(from: 2, to: numRows + 1, by: 1) {
             var row = [1]
-            for j in 0 ..< i - 1 {
-                row.append(rows[i - 1][j] + rows[i - 1][j + 1])
+            for j in stride(from: 0, to: i - 2, by: 1) {
+                row.append(rows[i - 2][j] + rows[i - 2][j + 1])
             }
             row.append(1)
             rows.append(row)
