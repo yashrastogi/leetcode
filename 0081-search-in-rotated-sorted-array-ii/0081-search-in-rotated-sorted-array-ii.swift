f2f49aeb763074f9f8b80d5f778b76e1b2c8d82e
Yash Rastogi <yashrastogi@gmail.com>
2024-04-06 18:44:55 +0530

    Time: 14 ms (54.76%), Space: 15.3 MB (57.14%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.swift
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.swift
@@ -1,27 +1,27 @@
 class Solution {
-  func search(_ nums: [Int], _ target: Int) -> Bool {
-    var lo = 0, hi = nums.count - 1
-    while lo <= hi {
-      let mid = (lo + hi) / 2
-      if nums[mid] == target {
-        return true
-      } else if nums[lo] == nums[mid] && nums[hi] == nums[mid] {
-        lo += 1
-        hi -= 1
-      } else if nums[lo] <= nums[mid] {
-        if target < nums[mid] && target >= nums[lo] {
-          hi = mid - 1
-        } else {
-          lo = mid + 1
+    func search(_ nums: [Int], _ target: Int) -> Bool {
+        var lo = 0, hi = nums.count - 1
+        while lo <= hi {
+            let mid = (lo + hi) / 2
+            if nums[mid] == target {
+                return true
+            } else if nums[lo] == nums[mid] && nums[hi] == nums[mid] {
+                lo += 1
+                hi -= 1
+            } else if nums[lo] <= nums[mid] {
+                if target < nums[mid] && target >= nums[lo] {
+                    hi = mid - 1
+                } else {
+                    lo = mid + 1
+                }
+            } else {
+                if target > nums[mid] && target <= nums[hi] {
+                    lo = mid + 1
+                } else {
+                    hi = mid - 1
+                }
+            }
         }
-      } else {
-        if target > nums[mid] && target <= nums[hi] {
-          lo = mid + 1
-        } else {
-          hi = mid - 1
-        }
-      }
+        return false
     }
-    return false
-  }
 }
