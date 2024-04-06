a7b5028ceb8e4de8db9798bcf1eea60044819810
Yash Rastogi <yashrastogi@gmail.com>
2024-04-06 18:33:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.swift
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.swift
@@ -1,22 +1,30 @@
 class Solution {
-  func search(_ nums: [Int], _ target: Int) -> Bool {
-    func binarySearch(_ lo: Int = 0, _ hi: Int = nums.count - 1) -> Bool {
-      guard lo <= hi else { return false }
-      let mid = (lo + hi) / 2
-      if nums[mid] == target {
-        return true
-      } else if nums[mid] == nums[lo] && nums[mid] == nums[hi] {
-        return binarySearch(lo + 1, hi - 1)
-      } else if nums[mid] >= nums[lo] && nums[mid] <= nums[hi] {
-        if nums[mid] < target {
-          return binarySearch(mid + 1, hi)
-        } else {
-          return binarySearch(lo, mid - 1)
+    func search(_ nums: [Int], _ target: Int) -> Bool {
+        func findHighest(_ lo: Int = 0, _ hi: Int = nums.count - 1) -> Int {
+            var maxNum = -Int.max, maxNumI = -1
+            for i in lo ... hi {
+                if nums[i] > maxNum {
+                    maxNum = nums[i]
+                    maxNumI = i
+                }
+            }
+            return maxNumI
         }
-      } else {
-        return binarySearch(lo, mid - 1) || binarySearch(mid + 1, hi)
-      }
+
+        func binarySearch(_ startIndex: Int, _ lo: Int = 0, _ hi: Int = nums.count - 1) -> Bool {
+            guard lo <= hi else { return false }
+            let mid = (lo + hi) / 2
+            let midIndex = (startIndex + mid) % nums.count
+            if nums[midIndex] == target {
+                return true
+            } else if nums[midIndex] < target {
+                return binarySearch(startIndex, mid + 1, hi)
+            } else {
+                return binarySearch(startIndex, lo, mid - 1)
+            }
+        }
+        
+        let startIndex = (findHighest() + 1) % nums.count
+        return binarySearch(startIndex)
     }
-    return binarySearch()
-  }
 }
