35e288a07cc3376d23be029bcfe786bc152a33fd
Yash Rastogi <yashrastogi@gmail.com>
2024-04-12 12:36:45 +0530

    Time: 38 ms (76.99%), Space: 17.4 MB (33.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.swift
+++ 0169-majority-element/0169-majority-element.swift
@@ -2,8 +2,9 @@ class Solution {
     func majorityElement(_ nums: [Int]) -> Int {
         var counter = [Int: Int]()
         for num in nums {
-            counter[num, default: 0] += 1
-            if counter[num]! > nums.count / 2 {
+            let temp = (counter[num] ?? 0) + 1
+            counter[num] = temp
+            if temp > nums.count / 2 {
                 return num
             }
         }
