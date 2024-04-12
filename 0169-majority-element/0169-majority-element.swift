96b28580367c693a61c5670dae2d5cb00dbec02b
Yash Rastogi <yashrastogi@gmail.com>
2024-04-12 13:05:55 +0530

    Time: 45 ms (49.42%), Space: 17.6 MB (21.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.swift
+++ 0169-majority-element/0169-majority-element.swift
@@ -1,14 +1,11 @@
 class Solution {
     func majorityElement(_ nums: [Int]) -> Int {
-        var lastNum = Int.max, lastNumCount = 0
+        var counter = [Int: Int]()
         for num in nums {
-            if lastNumCount == 0 {
-                lastNum = num
-                lastNumCount = 1
-            } else  {
-                lastNumCount += lastNum == num ? 1 : -1
-            }
+            let temp = (counter[num] ?? 0) + 1
+            counter[num] = temp
+            if temp > nums.count / 2 { return num }
         }
-        return lastNum
+        return 0
     }
 }
