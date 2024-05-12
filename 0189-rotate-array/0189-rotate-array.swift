5f38a748458b178258124fe428dc0c8008bdbc4a
Yash Rastogi <yashrastogi@gmail.com>
2024-05-12 11:35:15 +0530

    Time: 123 ms (77%), Space: 23.1 MB (64.17%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.swift
+++ 0189-rotate-array/0189-rotate-array.swift
@@ -1,7 +1,6 @@
 class Solution {
     func rotate(_ nums: inout [Int], _ k: Int) {
         var k = k % nums.count
-        let numReversed = Array(nums.reversed())
-        nums = numReversed[0..<k].reversed() + nums[0..<(nums.count-k)]
+        nums = Array(nums.reversed())[0..<k].reversed() + nums[0..<(nums.count-k)]
     }
 } 
