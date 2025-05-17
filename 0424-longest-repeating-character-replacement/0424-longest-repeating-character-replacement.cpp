493a43c4e37387457d22c7c69f33fc4f8fa613df
Yash Rastogi <yashrastogi@gmail.com>
2025-05-17 09:22:02 +0530

    Time: 7 ms (51.37%), Space: 10.9 MB (72.62%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        map<char, int> char_freq;
+        int max_len = 0, most_freq = 0;
+        for(int j = 0, i = 0; j< s.size(); j++) {
+            char_freq[s[j]]++;
+            most_freq = max(most_freq, char_freq[s[j]]);
+            int curr_window_length = j - i + 1;
+            if(curr_window_length - most_freq > k) {
+                char_freq[s[i]]--;
+                i++;
+                curr_window_length = j - i + 1;    
+            }
+            // for(int x = i; x <= j; x++) cout<<s[x]<<' ';cout<<endl;
+            max_len = max(max_len, curr_window_length);
+        }
+
+        return max_len;
+    }
+};
