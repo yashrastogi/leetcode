c80d851d3fdf6c74b00aa9559a5f515664c815d9
Yash Rastogi <yashrastogi@gmail.com>
2025-05-18 12:39:00 +0530

    Time: 25 ms (54.66%), Space: 134.3 MB (98.68%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,15 +4,15 @@ public:
         vector<int> res(nums.size() - k + 1);
         deque<int> dq;
         for (int left = 0, right = 0; right < nums.size(); right++) {
-            while (dq.size() > 0 && nums[right] >= nums[dq.back()]) 
+            while (dq.size() > 0 && nums[right] >= nums[dq.back()])
                 dq.pop_back();
             dq.push_back(right);
-            if (right - left + 1 > k) 
+            if (right - left + 1 > k) {
                 left++;
-            else if (right - left + 1 < k) 
+                if (dq.front() < left)
+                    dq.pop_front();
+            } else if (right - left + 1 < k)
                 continue;
-            while (dq.front() < left)
-                dq.pop_front();
             res[left] = nums[dq.front()];
         }
         return res;
