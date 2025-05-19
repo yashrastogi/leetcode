6f96bee787eb504fea856c2d72ed0be21433c5d1
Yash Rastogi <yashrastogi@gmail.com>
2025-05-19 12:51:25 +0530

    Time: 43 ms (80.4%), Space: 135.8 MB (99.07%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -2,31 +2,22 @@ class TimeMap {
 public:
     map<string, deque<pair<int, string>>> data;
     TimeMap() {}
-
+    
     void set(string key, string value, int timestamp) {
-        this->data[key].push_back({timestamp, value});
+        this->data[key].push_front({timestamp, value});
     }
-
+    
     string get(string key, int timestamp) {
-        deque<pair<int, string>>& target_vec = this->data[key];
-        int lo = 0, hi = target_vec.size() - 1;
-        string ans = "";
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            if (target_vec[mid].first > timestamp) {
-                // if timestamp of mid exceeds given, go left
-                hi = mid - 1;
-            } else if (timestamp == target_vec[mid].first) {
-                // if timestamp match found return value
-                return target_vec[mid].second;
-            } else {
-                // if data timestamp < given timestamp, temporarily store ans
-                // check if higher timestamp within range possible by going
-                // right
-                ans = target_vec[mid].second;
-                lo = mid + 1;
+        deque<pair<int, string>> &target_vec = this->data[key];
+        // cout << "[ ";
+        for(auto &pair: target_vec) {
+            // cout << pair.first << ' ';
+            if (pair.first <= timestamp) {
+                // cout << "]\n";
+                return pair.second;
             }
         }
-        return ans;
+        cout << "]\n";
+        return "";
     }
 };
