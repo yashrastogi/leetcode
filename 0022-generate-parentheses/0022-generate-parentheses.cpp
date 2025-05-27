77f2211d2a789c7a369abd36e26040531fab4c3c
Yash Rastogi <yashrastogi@gmail.com>
2025-05-27 22:22:57 +0530

    Time: 7 ms (12.02%), Space: 21.4 MB (6.63%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,57 +1,53 @@
 class Solution {
 public:
     vector<string> substrings;
-    
+
     vector<string> generateParenthesis(int n) {
-        stack<char> brackets;
-        recurse(0, 2 * n, "", 0, 0, brackets);
+        recurse(0, 2 * n, "", 0, 0);
         return substrings;
     }
 
-    void recurse(int index, int maxLen, string temp, int open, int close, stack<char> &brackets) { 
-        // ) stack size nil invalid string
-        if(index == maxLen) {
-            if(open == close && close == maxLen/2)
-                if(isBalanced(temp))
+    void recurse(int index, int maxLen, string temp, int open, int close) {
+        if (index == maxLen) {
+            if (open == close && close == maxLen / 2)
+                if (isBalanced(temp))
                     substrings.push_back(temp);
             return;
         }
-        // cout << temp << endl;
-        // open/close cant be more than n 
-        
-        
-        if(open + 1 <= maxLen / 2) {
-            brackets.push('(');
-            recurse(index + 1, maxLen, temp + "(", open + 1, close, brackets);
-            brackets.pop();
+
+        if (close < open) {
+            recurse(index + 1, maxLen, temp + ")", open, close + 1);
         }
-        if(close < open && close + 1 <= maxLen / 2) {
-            if(brackets.size()) {
-                recurse(index + 1, maxLen, temp + ")", open, close + 1, brackets);
-            }
+        if (open + 1 <= maxLen / 2) {
+            recurse(index + 1, maxLen, temp + "(", open + 1, close);
         }
     }
 
     bool isBalanced(string brackets) {
         stack<char> br;
-        for(char c: brackets) {
-            if(c == '(') br.push('(');
-            else if (br.size()) br.pop();
-            else return false;
+        for (char c : brackets) {
+            if (c == '(')
+                br.push('(');
+            else if (br.size())
+                br.pop();
+            else
+                return false;
         }
-        if(br.size()) return false;
+        if (br.size())
+            return false;
         return true;
     }
 
-    bool partialBalance(string str) { 
+    bool partialBalance(string str) {
         stack<char> br;
-        for(char c: str) {
-            if(c == '(') br.push('(');
-            else if (br.size()) br.pop();
-            else return false;
+        for (char c : str) {
+            if (c == '(')
+                br.push('(');
+            else if (br.size())
+                br.pop();
+            else
+                return false;
         }
         return true;
     }
-
 };
-
