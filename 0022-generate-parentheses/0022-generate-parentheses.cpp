b4a905d83db95d8b57e853a93dc742e5c52f36f1
Yash Rastogi <yashrastogi@gmail.com>
2025-05-27 22:08:45 +0530

    Time: 47 ms (5.05%), Space: 58.3 MB (5.05%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<string> substrings;
+    
+    vector<string> generateParenthesis(int n) {
+        recurse(0, 2 * n, "", 0, 0);
+        return substrings;
+    }
+
+    void recurse(int index, int maxLen, string temp, int open, int close) { 
+        // ) stack size nil invalid string
+        if(index == maxLen) {
+            if(open == close && close == maxLen/2)
+                if(isBalanced(temp))
+                    substrings.push_back(temp);
+            return;
+        }
+        // cout << temp << endl;
+        // open/close cant be more than n 
+        if(open + 1 <= maxLen / 2)
+            recurse(index + 1, maxLen, temp + "(", open + 1, close);
+        if(close + 1 <= maxLen / 2)
+            recurse(index + 1, maxLen, temp + ")", open, close + 1);
+    }
+
+    bool isBalanced(string brackets) {
+        stack<char> br;
+        for(char c: brackets) {
+            if(c == '(') br.push('(');
+            else if (br.size()) br.pop();
+            else return false;
+        }
+        if(br.size()) return false;
+        return true;
+    }
+
+    bool partialBalance(string str) { 
+        stack<char> br;
+        for(char c: str) {
+            if(c == '(') br.push('(');
+            else if (br.size()) br.pop();
+            else return false;
+        }
+        return true;
+    }
+
+};
+
