ed512a6bd82988c9ae73e265b56498e5cf87d1bf
Yash Rastogi <yashrastogi@gmail.com>
2025-05-28 15:44:37 +0530

    Time: 27 ms (42.12%), Space: 107.4 MB (42.1%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         vector<int> res(temperatures.size(), 0);
         stack<int> st;
-        for(int i = 0; i < temperatures.size(); i++) {
-            while(!st.empty() && temperatures[st.top()] < temperatures[i]) {
+        for (int i = 0; i < temperatures.size(); i++) {
+            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
                 res[st.top()] = i - st.top();
                 st.pop();
             }
