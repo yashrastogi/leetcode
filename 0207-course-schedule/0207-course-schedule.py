65b5ef3dd38bbca4366bc2782418c2d5c9ac9826
Yash Rastogi <yashrastogi@gmail.com>
2026-03-02 07:02:47 -0500

    Time: 7 ms (46.2%), Space: 21.7 MB (10.8%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,24 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def checkCycle(i):
-            nonlocal visited, visited_curr
+        visited = set()
+        visited_curr = set()
+
+        def checkCycle(i, visited=visited, visited_curr=visited_curr):
             visited.add(i)
             visited_curr.add(i)
             for nbr in graph[i]:
-                if nbr not in visited and checkCycle(nbr):
-                    return True
-                elif nbr in visited_curr:
+                if nbr in visited_curr or (nbr not in visited and checkCycle(nbr)):
                     return True
             visited_curr.remove(i)
             return False
-        
+
         graph = defaultdict(list)
         for p in prerequisites:
             graph[p[0]].append(p[1])
-        visited = set()
-        visited_curr = set()
         for i in range(numCourses):
-             if i not in visited:
-                    if checkCycle(i):
-                        return False
+            if i not in visited:
+                if checkCycle(i):
+                    return False
         return True
