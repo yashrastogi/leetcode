e84c60accc64ce6b7da0b99eb980f760b33fa612
Yash Rastogi <yashrastogi@gmail.com>
2026-03-20 15:52:16 -0400

    Time: 926 ms (17.97%), Space: 70.8 MB (13.69%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,21 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = {}
+
+        def rec(d):
+            if d == 0:
+                return 0
+            elif d < 0:
+                return float("inf")
+            elif d in memo:
+                return memo[d]
+            ret = float("inf")
+            for de in coins:
+                ret = min(ret, 1 + rec(d - de))
+            memo[d] = ret
+            return ret
+
+        r = rec(amount)
+        if r == float("inf"):
+            return -1
+        return r
