6426129792596898c978a743f547bb8ecf3f9306
Yash Rastogi <yashrastogi@gmail.com>
2026-03-20 16:04:24 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,22 +1,36 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort(reverse=True)
-        memo = {}
+        def topDown():
+            memo = {}
 
-        def rec(d):
-            if d == 0:
-                return 0
-            elif d < 0:
-                return float("inf")
-            elif d in memo:
-                return memo[d]
-            ret = float("inf")
-            for de in coins:
-                ret = min(ret, 1 + rec(d - de))
-            memo[d] = ret
-            return ret
+            def rec(d):
+                if d == 0:
+                    return 0
+                elif d < 0:
+                    return float("inf")
+                elif d in memo:
+                    return memo[d]
+                ret = float("inf")
+                for de in coins:
+                    ret = min(ret, 1 + rec(d - de))
+                memo[d] = ret
+                return ret
 
-        r = rec(amount)
-        if r == float("inf"):
-            return -1
-        return r
+            r = rec(amount)
+            if r == float("inf"):
+                return -1
+            return r
+        
+        def bottomUp():
+            dp = [float('inf')] * (amount + 1)
+            dp[0] = 0
+            for i in range(1, len(dp)):
+                for coin in coins:
+                    if i - coin >= 0:
+                        dp[i] = min(dp[i], 1 + dp[i - coin])
+            r = dp[amount]
+            if r == float("inf"):
+                return -1
+            return r
+            
+        return bottomUp()
