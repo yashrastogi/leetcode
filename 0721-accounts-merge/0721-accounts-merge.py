e489adee8a354df4817fd0e3503e71fc985e8d97
Yash Rastogi <yashrastogi@gmail.com>
2026-04-01 10:33:30 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,20 +1,8 @@
-from pprint import pprint
-
-
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        m = {}
-        adj = defaultdict(list)
-        for ac in accounts:
-            for i in range(1, len(ac)):
-                if ac[i] not in adj:
-                    adj[ac[i]] = []
-                for j in range(i + 1, len(ac)):
-                    adj[ac[i]].append(ac[j])
-                m[ac[i]] = ac[0]
-
-        parent = {p: p for p in adj}
-        rank = {p: 0 for p in adj}
+        parent = {}
+        rank = {}
+        m = {} # Maps email to account name
 
         def find(u):
             if parent[u] != u:
@@ -25,7 +13,7 @@ class Solution:
             p_u = find(u)
             p_v = find(v)
             if p_u == p_v:
-                return False  # already connected
+                return False 
             if rank[p_u] < rank[p_v]:
                 parent[p_u] = p_v
             elif rank[p_u] > rank[p_v]:
@@ -35,15 +23,33 @@ class Solution:
                 rank[p_u] += 1
             return True
 
-        for e1 in adj:
-            for e2 in adj[e1]:
-                union(e1, e2)
+        # 1. Initialize nodes and union them in one O(N * K) pass
+        for ac in accounts:
+            name = ac[0]
+            first_email = ac[1] if len(ac) > 1 else None
+            
+            for i in range(1, len(ac)):
+                email = ac[i]
+                # Initialize in DSU if not seen before
+                if email not in parent:
+                    parent[email] = email
+                    rank[email] = 0
+                
+                m[email] = name
+                
+                # Union every email to the FIRST email in this specific account list
+                if i > 1:
+                    union(first_email, email)
 
+        # 2. Gather emails by their absolute root
         ret = defaultdict(list)
         for em in m:
             ret[find(em)].append(em)
+
+        # 3. Format and sort the final output
         ret_list = []
-        for em in ret:
-            obj = [m[em], *sorted(ret[em])]
+        for root in ret:
+            obj = [m[root]] + sorted(ret[root])
             ret_list.append(obj)
+            
         return ret_list
